a2aba790af58f44d8eb57fbe932d0bf6c1e10c88
mszpak <moszee@gmail.com>
2016-06-05 10:31:38 +0200

    Initial project commit

---
+++ src/main/java/org/szpax/TreningPlanApplication.java
@@ -0,0 +1,12 @@
+package org.szpax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreningPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreningPlanApplication.class, args);
+	}
+}
